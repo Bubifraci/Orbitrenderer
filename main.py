acc997700edf6bcde7ad1673296083d8718a02e4
danie <daniel_2006@outlook.com>
2025-11-19 22:40:59 +0100

    Add double renderer

--- main.py
+++ main.py
@@ -22,41 +22,34 @@ def solve_kepler(M, e, tol=1e-10, max_iter=100):
     raise RuntimeError("Newton iteration fehlgeschlagen")
 
 def maneuver(pl, ba, dv):
-    where = input("Wo soll das Manöver durchgeführt werden? (a für Apogäum, p für Perigäum)").lower()
-    if(where != "a" or where != "p"):
+    where = input("Wo soll das Manöver durchgeführt werden? (a für Apogäum, p für Perigäum): ")
+    if(where.lower() != "a" and where.lower() != "p"):
         return maneuver(pl, ba, dv)
     if(dv == None):
-        dvNew = input("Wie lautet das dv?")
+        dvNew = input("Wie lautet das dv (in m/s)?: ")
         if(dvNew.isnumeric()):
             dv = float(dvNew)
         else:
             return maneuver(pl, ba, dv)
-    
-    vi = ba.getSpeedAtPoint(ba.ra)
 
-    if(where == "p"):
-        return None
-
-def startSatellite(pl, ba, timeMultiplier = 50000, iterations = None):
-    renderer.init()
-
-    scale = 75000
-
-    posX = None
-    posY = None
-
-    oldX = 0
-    oldY = 0
-
+    r = 0.0
     vals = values.Values()
+    if(where == "p"):
+        r = ba.rp
+    else:
+        r = ba.ra
+    newV = ba.getSpeedAtPoint(r) + dv
+    newA = 1.0/(-(pow(newV, 2)/(vals.G*pl.mass)) + 2.0/r)
+    otherR = 2*newA-r
 
-    start = time.time()
-    curTime = start
-    repeat = True
-    i = 0
-    while (repeat):
-        #Calculate  next step
-        i += 1
+    newBahn = None
+    if(where == "p"):
+        newBahn = bahn.Bahn(pl, r/1000, otherR/1000, ba.inclination, ba.raan, ba.w, ba.anomaly, ba.tp)
+    else:
+        newBahn = bahn.Bahn(pl, otherR/1000, r/1000, ba.inclination, ba.raan, ba.w, ba.anomaly, ba.tp)
+    startSatellite(pl, ba, ba2=newBahn)
+
+def calculateCoordinates(pl, ba, title, curTime, start, oldX, oldY):
         M = ba.calculateMeanAnomaly(curTime-start)
         E = solve_kepler(M, ba.e)
         pos = ba.getVector(E)
@@ -72,9 +65,37 @@ def startSatellite(pl, ba, timeMultiplier = 50000, iterations = None):
         velocity = sqrt(pow(velX, 2) + pow(velY, 2))
         velocity1 = ba.getSpeedAtPoint(distance)
 
-        print(f"X: {posX:.3f}; Y: {posY:.3f}; At time t={curTime-start:.2f}s; Distance from planet: {distance/1000:.3f}km; Velocity: tangential -> {velocity/1000:.3f}km/s or vis-viva -> {velocity1/1000:.3f}km/s")
+        print(f"{title}: X: {posX:.3f}; Y: {posY:.3f}; At time t={curTime-start:.2f}s; Distance from planet: {distance/1000:.3f}km; Velocity: tangential -> {velocity/1000:.3f}km/s or vis-viva -> {velocity1/1000:.3f}km/s")
+        return[posX, posY]
+    
 
-        renderer.render(posX, posY, scale, pl.radius)
+def startSatellite(pl, ba, timeMultiplier = 50000, iterations = None, ba2 = None):
+    renderer.init()
+
+    scale = 75000
+
+    oldX = 0
+    oldY = 0
+    oldX2 = 0
+    oldY2 = 0
+
+    vals = values.Values()
+
+    start = time.time()
+    curTime = start
+    repeat = True
+    i = 0
+    while (repeat):
+        i += 1
+        posX, posY = calculateCoordinates(pl, ba, "Original", curTime, start, oldX, oldY)
+        if(ba2 != None):
+            posX2, posY2 = calculateCoordinates(pl, ba2, "New", curTime, start, oldX2, oldY2)
+            renderer.render(posX, posY, scale, pl.radius, posX2, posY2)
+            oldX2 = posX2
+            oldY2 = posY2
+        else:
+            renderer.render(posX, posY, scale, pl.radius)
+        
 
         curTime += 1 #Add a second
         if(iterations != None):
@@ -84,6 +105,7 @@ def startSatellite(pl, ba, timeMultiplier = 50000, iterations = None):
         oldX = posX
         oldY = posY
 
+
 pl = planet.Planet("Erde", 6378, 5.972e24)
 ba = bahn.Bahn(pl, 200, 600, 0, 0, 0)
-startSatellite(pl, ba)
+maneuver(pl, ba, None)
--- renderer.py
+++ renderer.py
@@ -4,7 +4,7 @@ import sys
 screen_width = 800
 screen_height = 600
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = None
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -15,15 +15,23 @@ centerX = screen_width/2
 centerY = screen_height/2
 
 def init():
+    global screen
+    screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.init()
 
 
-def render(posX, posY, scale, plRadius):
+def render(posX, posY, scale, plRadius, posX2 = None, posY2 = None):
+    global screen
     screen.fill((0, 0, 0))
 
     x = int(centerX + posX / scale)
     y = int(centerY + posY / scale)
 
-    pygame.draw.circle(screen, RED, (x, y), 4)
+    pygame.draw.circle(screen, WHITE, (x, y), 4)
+    if(posX2 != None and posY2 != None):
+        x2 = int(centerX + posX2 / scale)
+        y2 = int(centerY + posY2 / scale)
+        pygame.draw.circle(screen, RED, (x2, y2), 4)
+
     pygame.draw.circle(screen, BLUE, (centerX, centerY), plRadius/1000)
     pygame.display.update()
